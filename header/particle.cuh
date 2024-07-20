8e0001d2f887c267fbfad61d5b3dce37f0c5d2f2
Sriram katta <sriram.katta@fau.de>
2024-07-20 22:54:37 +0200

    renamed variables

--- header/particle.cuh
+++ header/particle.cuh
@@ -7,9 +7,9 @@ struct Particle
   vec3d pos = make_double3(0.0, 0.0, 0.0);   // Position
   vec3d vel = make_double3(0.0, 0.0, 0.0);   // Velocity
   vec3d force = make_double3(0.0, 0.0, 0.0); // Force
-  double mass{};                               // mass
-  double radius{};                             // radius
-  bool fix{};                                  // boundary type
-  double density{};                            // density
-  double pressure{};                           // pressure
+  double mass{};                             // mass
+  double radius{};                           // radius
+  bool ghost{};                              // boundary type
+  double density{};                          // density
+  double pressure{};                         // pressure
 };
