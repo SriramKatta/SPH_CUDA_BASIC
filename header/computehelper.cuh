bc623fc33f7fcdfe4ef2112977dea570a94404d4
Sriram katta <sriram.katta@fau.de>
2024-07-21 00:13:04 +0200

    refactor done

--- header/computehelper.cuh
+++ header/computehelper.cuh
@@ -2,6 +2,7 @@
 
 #include "particle.cuh"
 #include "vector_helper.cuh"
+#include "kernel.cuh"
 
 using vec3d = double3;
 using vec3i = int3;
@@ -23,32 +24,32 @@ __device__ int getCellIndex1D(const vec3i &cellIndex, int nCellsPerSide)
 }
 
 // CUDA kernel to initialize cellHead
-__global__ void initializeCellHeadKernel(int *d_cellHead, int nCells)
+__global__ void setCellDS(int *CellDS, int nCells)
 {
   GRID_STRIDE(index, stride);
   for (int i = index; i < nCells; i += stride)
   {
-    d_cellHead[i] = -1;
+    CellDS[i] = -1;
   }
 }
 
 // CUDA kernel to initialize cellNext
-__global__ void initializeCellNextKernel(int *d_cellNext, int nParticles)
+__global__ void setPartDS(int *partDs, int nParticles)
 {
   GRID_STRIDE(index, stride);
   for (int i = index; i < nParticles; i += stride)
   {
-    d_cellNext[i] = -1;
+    partDs[i] = -1;
   }
 }
 
 // CUDA kernel to build linked lists
-__global__ void buildLinkedListsKernel(Particle *d_particles,
-                                       int *CellDs,
-                                       int *PartDS,
-                                       double cellSize,
-                                       int nCellsPerSide,
-                                       int nParticles)
+__global__ void updateDS(Particle *d_particles,
+                         int *CellDs,
+                         int *PartDS,
+                         double cellSize,
+                         int nCellsPerSide,
+                         int nParticles)
 {
   GRID_STRIDE(index, stride);
   for (int i = index; i < nParticles; i += stride)
@@ -59,66 +60,65 @@ __global__ void buildLinkedListsKernel(Particle *d_particles,
   }
 }
 
-// CUDA kernel to compute densities
-__global__ void computeDensitiesKernel(Particle *d_particles,
-                                       int *d_cellHead,
-                                       int *d_cellNext,
-                                       double cellSize,
-                                       int nCellsPerSide,
-                                       double h,
-                                       int nParticles,
-                                       double restDensity,
-                                       double gasConstant)
+__global__ void updatePressureDensity(Particle *d_particles,
+                                      int *CellDs,
+                                      int *partDs,
+                                      double cellSize,
+                                      int nCellsPerSide,
+                                      int nParticles,
+                                      double h,
+                                      double restDensity,
+                                      double gasConstant,
+                                      kernel *k)
 {
-  double weightA = 315.0 / (64.0 * M_PI * pow(h, 9));
-  double h2 = h * h;
 
   GRID_STRIDE(index, stride);
-
   for (int i = index; i < nParticles; i += stride)
   {
-    double density = restDensity;
-    if (d_particles[i].fix == false)
+    if (d_particles[i].ghost != false)
     {
-      density = 0;
-      vec3i cellIndex = getCellIndex(d_particles[i].pos, cellSize, nCellsPerSide);
-
-      for (int ix = -1; ix <= 1; ++ix)
-      {
-        for (int iy = -1; iy <= 1; ++iy)
-        {
-          for (int iz = -1; iz <= 1; ++iz)
-          {
-            int3 neighborCellIndex = make_int3((cellIndex.x + ix + nCellsPerSide) % nCellsPerSide,
-                                               (cellIndex.y + iy + nCellsPerSide) % nCellsPerSide,
-                                               (cellIndex.z + iz + nCellsPerSide) % nCellsPerSide);
-
-            int neighborCellId = getCellIndex1D(neighborCellIndex, nCellsPerSide);
-
-            int neighborIdx = d_cellHead[neighborCellId];
-            while (neighborIdx != -1)
-            {
-              double r2 = normSquared(d_particles[i].pos - d_particles[neighborIdx].pos);
-              double weightB = pow(h2 - r2, 3);
-
-              if (r2 <= h2)
-              {
-                density += d_particles[neighborIdx].mass * weightA * weightB;
-              }
-
-              neighborIdx = d_cellNext[neighborIdx];
-            }
-          }
-        }
-      }
+      d_particles[i].density = restDensity;
+      d_particles[i].pressure = 0.0;
+      continue;
     }
+
+    double density = 0;
+    vec3i cellIndex = getCellIndex(d_particles[i].pos, cellSize, nCellsPerSide);
+
+    NBD_LOOP(
+        int3 neighborCellIndex = make_int3((cellIndex.x + ix + nCellsPerSide) % nCellsPerSide,
+                                           (cellIndex.y + iy + nCellsPerSide) % nCellsPerSide,
+                                           (cellIndex.z + iz + nCellsPerSide) % nCellsPerSide);
+
+        int neighborCellId = getCellIndex1D(neighborCellIndex, nCellsPerSide);
+
+        int neighborIdx = CellDs[neighborCellId];
+        while (neighborIdx != -1) {
+          const vec3d rij = d_particles[i].pos - d_particles[neighborIdx].pos;
+          double r = norm(rij);
+          if (r <= h)
+          {
+            density += d_particles[neighborIdx].mass * k->W(r);
+          }
+          neighborIdx = partDs[neighborIdx];
+        })
+
     d_particles[i].density = density;
-    d_particles[i].pressure = gasConstant * (density - restDensity); 
+    d_particles[i].pressure = gasConstant * (density - restDensity);
   }
 }
 
 // CUDA kernel to compute forces
-__global__ void calculateForcesKernel(Particle *d_particles, int *d_cellHead, int *d_cellNext, double cellSize, int nCellsPerSide, double h, int nParticles, double viscosityCoefficient, double gravity)
+__global__ void updateAcceleration(Particle *d_particles,
+                                   int *d_cellHead,
+                                   int *d_cellNext,
+                                   double cellSize,
+                                   int nCellsPerSide,
+                                   double h,
+                                   int nParticles,
+                                   double viscosityCoefficient,
+                                   double gravity,
+                                   kernel *k)
 {
   double weightA = -45.0 / (M_PI * pow(h, 6));
 
@@ -126,7 +126,7 @@ __global__ void calculateForcesKernel(Particle *d_particles, int *d_cellHead, in
 
   for (int i = index; i < nParticles; i += stride)
   {
-    if (d_particles[i].fix == false)
+    if (d_particles[i].ghost == false)
     {
       double fx = 0.0;
       double fy = 0.0;
@@ -168,7 +168,7 @@ __global__ void calculateForcesKernel(Particle *d_particles, int *d_cellHead, in
                   fz += pressureTerm * pressureGradient * dist_z / (r); // + 0.01 * h * h);
 
                   // Viscosity force
-                  if (d_particles[neighborIdx].fix == false)
+                  if (d_particles[neighborIdx].ghost == false)
                   {
                     double viscosityLaplacian = -weightA * weightB;
                     double viscosityTerm_x = viscosityCoefficient * d_particles[neighborIdx].mass * (d_particles[neighborIdx].vel.x - d_particles[i].vel.x) / d_particles[neighborIdx].density;
@@ -199,12 +199,10 @@ __global__ void updateState(Particle *d_particles, double dt, int n)
   GRID_STRIDE(index, stride);
   for (int i = index; i < n; i += stride)
   {
-    if (d_particles[i].fix == false)
+    if (!d_particles[i].ghost)
     {
       d_particles[i].vel += d_particles[i].force * (dt / d_particles[i].density);
       d_particles[i].pos += d_particles[i].vel * dt;
     }
   }
 }
-
-
--- impl/main.cu
+++ impl/main.cu
@@ -15,6 +15,7 @@
 #include "cuda_helper.cuh"
 #include "thrust/host_vector.h"
 #include "thrust/device_vector.h"
+#include "kernel.cuh"
 
 const std::string inputFilename = "../scenes/scene_2.vtk";
 const int nParticles = 480470;
@@ -23,73 +24,82 @@ const double dt = 0.000625;
 const double boxSize = 3.0;
 const double h = 0.045;
 const int nSteps = 8000;
-const int vtkOutputFrequency;
+const int vtkOutputFrequency = 100;
 const double gravity = 9.81;
 const double restDensity = 1000.0;
 const double viscosityCoefficient = 50.0;
 const double gasConstant = 20.0;
 
-
 // function to read particles data from a VTK file
-void readVTK(const std::string &filename, int numParticles, thrust::host_vector<Particle>& particles) {
+void readVTK(const std::string &filename, int numParticles, thrust::host_vector<Particle> &particles)
+{
     std::string line;
     int numPoints;
 
     // open file for reading
     std::ifstream file(inputFilename);
-    if (!file.is_open()) {
+    if (!file.is_open())
+    {
         std::cerr << "Error opening file: " << filename << std::endl;
         return;
     }
 
     // skip header lines
-    for (int i = 0; i < 4; ++i) {
+    for (int i = 0; i < 4; ++i)
+    {
         std::getline(file, line);
     }
 
     // points (positions)
-    file >> line >> numPoints >> line;      // POINTS numPoints float
-    if (numPoints != numParticles) {
+    file >> line >> numPoints >> line; // POINTS numPoints float
+    if (numPoints != numParticles)
+    {
         std::cerr << "Number of points in file does not match the provided number of particles." << std::endl;
         return;
     }
-    for (int i = 0; i < numParticles; ++i) {
+    for (int i = 0; i < numParticles; ++i)
+    {
         file >> particles[i].pos.x >> particles[i].pos.y >> particles[i].pos.z;
-        //std::cout << particles[i].x << std::endl;
+        // std::cout << particles[i].x << std::endl;
     }
 
     // skip to the POINT_DATA section
-    while (std::getline(file, line) && line.substr(0, 10) != "POINT_DATA");
+    while (std::getline(file, line) && line.substr(0, 10) != "POINT_DATA")
+        ;
 
     // masses
-    file >> line >> line >> line;           // SCALARS mass double
-    file >> line >> line;                   // LOOKUP_TABLE default
-    for (int i = 0; i < numParticles; ++i) {
+    file >> line >> line >> line; // SCALARS mass double
+    file >> line >> line;         // LOOKUP_TABLE default
+    for (int i = 0; i < numParticles; ++i)
+    {
         file >> particles[i].mass;
-        //std::cout << "mass " << particles[i].mass << std::endl;
+        // std::cout << "mass " << particles[i].mass << std::endl;
     }
 
     // velocities
-    file >> line >> line >> line;           // VECTORS velocity double
-    for (int i = 0; i < numParticles; ++i) {
+    file >> line >> line >> line; // VECTORS velocity double
+    for (int i = 0; i < numParticles; ++i)
+    {
         file >> particles[i].vel.x >> particles[i].vel.y >> particles[i].vel.z;
-        //std::cout << "velocity x " << particles[i].vx <<std::endl;
+        // std::cout << "velocity x " << particles[i].vx <<std::endl;
     }
 
     // radii
-    file >> line >> line >> line;           // SCALARS radius double
-    file >> line >> line;                   // LOOKUP_TABLE default
-    for (int i = 0; i < numParticles; ++i) {
+    file >> line >> line >> line; // SCALARS radius double
+    file >> line >> line;         // LOOKUP_TABLE default
+    for (int i = 0; i < numParticles; ++i)
+    {
         file >> particles[i].radius;
-        //std::cout << "radius " << particles[i].radius << std::endl;
+        // std::cout << "radius " << particles[i].radius << std::endl;
     }
 
     // fix
-    file >> line >> line >> line;           // SCALARS radius double
-    file >> line >> line;                   // LOOKUP_TABLE default
-    for (int i = 0; i < numParticles; ++i) {
+    file >> line >> line >> line; // SCALARS radius double
+    file >> line >> line;         // LOOKUP_TABLE default
+    for (int i = 0; i < numParticles; ++i)
+    {
         file >> particles[i].ghost;
-        //std::cout << "fix " << particles[i].ghost << std::endl;
+        // std::cout << "fix " << particles[i].ghost << std::endl;
     }
 
     // close file
@@ -97,7 +107,8 @@ void readVTK(const std::string &filename, int numParticles, thrust::host_vector<
 }
 
 // function to write particles data to a VTK file
-void writeVTK(thrust::host_vector<Particle>& particles, int step, int nParticles) {
+void writeVTK(thrust::host_vector<Particle> &particles, int step, int nParticles)
+{
     // open file for writing
     std::ostringstream filename;
     filename << "./output/" << "particles_" << std::setw(4) << std::setfill('0') << step << ".vtk";
@@ -111,8 +122,10 @@ void writeVTK(thrust::host_vector<Particle>& particles, int step, int nParticles
 
     // points section
     file << "POINTS " << nParticlesFluid << " float\n";
-    for (int i = 0; i < nParticles; i++) {
-        if (particles[i].ghost == false) {
+    for (int i = 0; i < nParticles; i++)
+    {
+        if (particles[i].ghost == false)
+        {
             file << particles[i].pos.x << " " << particles[i].pos.y << " " << particles[i].pos.z << "\n";
         }
     }
@@ -127,8 +140,10 @@ void writeVTK(thrust::host_vector<Particle>& particles, int step, int nParticles
     // mass
     file << "SCALARS m float\n";
     file << "LOOKUP_TABLE default\n";
-    for (int i = 0; i < nParticles; i++) {
-        if (particles[i].ghost == false) {
+    for (int i = 0; i < nParticles; i++)
+    {
+        if (particles[i].ghost == false)
+        {
             file << particles[i].mass << "\n";
         }
     }
@@ -136,8 +151,10 @@ void writeVTK(thrust::host_vector<Particle>& particles, int step, int nParticles
     // density
     file << "SCALARS rho float\n";
     file << "LOOKUP_TABLE default\n";
-    for (int i = 0; i < nParticles; i++) {
-        if (particles[i].ghost == false) {
+    for (int i = 0; i < nParticles; i++)
+    {
+        if (particles[i].ghost == false)
+        {
             file << particles[i].density << "\n";
         }
     }
@@ -145,42 +162,45 @@ void writeVTK(thrust::host_vector<Particle>& particles, int step, int nParticles
     // radii
     file << "SCALARS r float\n";
     file << "LOOKUP_TABLE default\n";
-    for (int i = 0; i < nParticles; i++) {
-        if (particles[i].ghost == false) {
+    for (int i = 0; i < nParticles; i++)
+    {
+        if (particles[i].ghost == false)
+        {
             file << particles[i].radius << "\n";
         }
     }
 
     // positions
     file << "VECTORS p float\n";
-    for (int i = 0; i < nParticles; i++) {
-        if (particles[i].ghost == false) {
+    for (int i = 0; i < nParticles; i++)
+    {
+        if (particles[i].ghost == false)
+        {
             file << particles[i].pos.x << " " << particles[i].pos.y << " " << particles[i].pos.z << "\n";
         }
     }
 
     // velocities
     file << "VECTORS v float\n";
-    for (int i = 0; i < nParticles; i++) {
-        if (particles[i].ghost == false) {
+    for (int i = 0; i < nParticles; i++)
+    {
+        if (particles[i].ghost == false)
+        {
             file << particles[i].vel.x << " " << particles[i].vel.y << " " << particles[i].vel.z << "\n";
         }
     }
 }
 
-
-
-
-
-int main() {
-    // initialize host variables I
-    //readConfig();
+int main()
+{
+    kernel k(h);
+    kernel *d_k;
+    cudaMalloc(&d_k, sizeof(kernel));
+    cudaMemcpy(d_k, &k, sizeof(kernel), cudaMemcpyHostToDevice);
 
     // allocate host memory
     thrust::host_vector<Particle> particles(nParticles);
-    //cudaMallocHost(&particles, nParticles * sizeof(Particle));
-
-    // initialize host variables II
+    
     readVTK(inputFilename, nParticles, particles);
     int nCellsPerSide = static_cast<int>(boxSize / h);
     int nCells = nCellsPerSide * nCellsPerSide * nCellsPerSide;
@@ -188,71 +208,47 @@ int main() {
 
     // allocate device memory
     thrust::device_vector<Particle> d_particles_vec = particles;
-    thrust::device_vector<int> d_cellHead_vec(nCells);
-    thrust::device_vector<int> d_cellNext_vec(nParticles);
-    Particle* d_particles = thrust::raw_pointer_cast(d_particles_vec.data());
-    int* d_cellHead = thrust::raw_pointer_cast(d_cellHead_vec.data());
-    int* d_cellNext = thrust::raw_pointer_cast(d_cellNext_vec.data());
-    //cudaMalloc(&d_particles, nParticles * sizeof(Particle));
-    //cudaMalloc(&d_cellHead, nCells * sizeof(int));
-    //cudaMalloc(&d_cellNext, nParticles * sizeof(int));
-//
-    //// copy particle data to device
-    //checkCuda(cudaMemcpy(d_particles, particles, nParticles * sizeof(Particle), cudaMemcpyHostToDevice), 11);
+    thrust::device_vector<int> CellDS_vec(nCells);
+    thrust::device_vector<int> PartDS_vec(nParticles);
+    Particle *d_particles = thrust::raw_pointer_cast(d_particles_vec.data());
+    int *d_cell_ptr = thrust::raw_pointer_cast(CellDS_vec.data());
+    int *d_part_ptr = thrust::raw_pointer_cast(PartDS_vec.data());
+    
+    auto [thPerBlk, blks] = setgpuconfig();
 
-    // block-thread-partitioning
-    //int threadsPerBlock = 256;
-    //int blocksPerGridParticles = (nParticles + threadsPerBlock - 1) / threadsPerBlock;
-    //int blocksPerGridCells = (nCells + threadsPerBlock - 1) / threadsPerBlock;
-    auto [threadsPerBlock, blocksPerGridParticles] = setgpuconfig();
-
-    // time start point
-    auto start = std::chrono::steady_clock::now();
-
-    // simulation step loop
-    for (int step = 0; step < nSteps; ++step) {
+    for (int step = 0; step < nSteps; ++step)
+    {
         // initialize cells on device
-        initializeCellHeadKernel<<<blocksPerGridParticles, threadsPerBlock>>>(d_cellHead, nCells);
-        initializeCellNextKernel<<<blocksPerGridParticles, threadsPerBlock>>>(d_cellNext, nParticles);
-        checkCuda(cudaGetLastError(), 1);
+        setCellDS<<<blks, thPerBlk>>>(d_cell_ptr, nCells);
+        checkCuda(cudaGetLastError(), __LINE__ - 1);
+        setPartDS<<<blks, thPerBlk>>>(d_part_ptr, nParticles);
+        checkCuda(cudaGetLastError(), __LINE__ - 1);
 
         // build linked lists on device
-        buildLinkedListsKernel<<<blocksPerGridParticles, threadsPerBlock>>>(d_particles, d_cellHead, d_cellNext, cellSize, nCellsPerSide, nParticles);
-        checkCuda(cudaGetLastError(), 2);
+        updateDS<<<blks, thPerBlk>>>(d_particles, d_cell_ptr, d_part_ptr, cellSize, nCellsPerSide, nParticles);
+        checkCuda(cudaGetLastError(), __LINE__ - 1);
 
         // compute densities
-        computeDensitiesKernel<<<blocksPerGridParticles, threadsPerBlock>>>(d_particles, d_cellHead, d_cellNext, cellSize, nCellsPerSide, h, nParticles, restDensity, gasConstant);
-        checkCuda(cudaGetLastError(), 3);
+        updatePressureDensity<<<blks, thPerBlk>>>(d_particles, d_cell_ptr, d_part_ptr, cellSize, nCellsPerSide, nParticles, h,restDensity, gasConstant, d_k);
+        checkCuda(cudaGetLastError(), __LINE__ - 1);
 
         // calculate forces
-        calculateForcesKernel<<<blocksPerGridParticles, threadsPerBlock>>>(d_particles, d_cellHead, d_cellNext, cellSize, nCellsPerSide, h, nParticles, viscosityCoefficient, gravity);
-        checkCuda(cudaGetLastError(), 4);
+        updateAcceleration<<<blks, thPerBlk>>>(d_particles, d_cell_ptr, d_part_ptr, cellSize, nCellsPerSide, h, nParticles, viscosityCoefficient, gravity, d_k);
+        checkCuda(cudaGetLastError(), __LINE__ - 1);
 
         // symplectic Euler Integration
-        updateState<<<blocksPerGridParticles, threadsPerBlock>>>(d_particles, dt, nParticles);
-        checkCuda(cudaGetLastError(), 5);
+        updateState<<<blks, thPerBlk>>>(d_particles, dt, nParticles);
+        checkCuda(cudaGetLastError(), __LINE__ - 1);
 
         // copy particles back to host for VTK output
-        if (step % vtkOutputFrequency == 0) {
-            //checkCuda(cudaMemcpy(particles, d_particles, nParticles * sizeof(Particle), cudaMemcpyDeviceToHost), 12);
+        if (step % vtkOutputFrequency == 0)
+        {
+            // checkCuda(cudaMemcpy(particles, d_particles, nParticles * sizeof(Particle), cudaMemcpyDeviceToHost), 12);
             particles = d_particles_vec;
-            //writeVTK(particles, step, nParticles);
+            // writeVTK(particles, step, nParticles);
             writeVTK(particles, step, nParticles);
             std::cout << "Step " << step << " done." << std::endl;
         }
     }
-
-    // time measurement
-    auto end = std::chrono::steady_clock::now();
-    std::chrono::duration<double, std::nano> elapsed_seconds = (end - start);
-    double meanExecutionTime = elapsed_seconds.count() / nSteps / 1e6;
-    std::cout << "Simulation complete." << std::endl;
-    std::cout << "Mean execution time per time step: " << meanExecutionTime << " ms" << std::endl;
-
-    // free memory
-    //cudaFree(d_particles);
-    //cudaFree(d_cellHead);
-    //cudaFree(d_cellNext);
-
     return 0;
 }
