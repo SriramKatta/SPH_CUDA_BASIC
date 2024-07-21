25eb3a645bb42bd4ccb9bd503a8517bfad6fd4cd
Sriram katta <sriram.katta@fau.de>
2024-07-21 09:25:19 +0200

    updated support radius for for simu

--- impl/main.cu
+++ impl/main.cu
@@ -12,7 +12,7 @@ const int nParticles = 480470;
 const int nParticlesFluid = 65220;
 const double dt = 0.000625;
 const double boxSize = 3.0;
-const double h = 0.045;
+const double h = 0.065;
 const int nSteps = 8000;
 const int vtkOutputFrequency = 100;
 const double gravity = 9.81;
@@ -36,7 +36,6 @@ int main()
     int nCells = nCellsPerSide * nCellsPerSide * nCellsPerSide;
     double cellSize = boxSize / nCellsPerSide;
 
-    // allocate device memory
     thrust::device_vector<Particle> d_particles_vec = particles;
     thrust::device_vector<int> CellDS_vec(nCells);
     thrust::device_vector<int> PartDS_vec(nParticles);
@@ -48,34 +47,27 @@ int main()
 
     for (int step = 0; step < nSteps; ++step)
     {
-        // initialize cells on device
         setCellDS<<<blks, thPerBlk>>>(d_cell_ptr, nCells);
         checkCuda(cudaGetLastError(), __LINE__ - 1);
         setPartDS<<<blks, thPerBlk>>>(d_part_ptr, nParticles);
         checkCuda(cudaGetLastError(), __LINE__ - 1);
 
-        // build linked lists on device
         updateDS<<<blks, thPerBlk>>>(d_particles, d_cell_ptr, d_part_ptr, cellSize, nCellsPerSide, nParticles);
         checkCuda(cudaGetLastError(), __LINE__ - 1);
 
-        // compute densities
         updatePressureDensity<<<blks, thPerBlk>>>(d_particles, d_cell_ptr, d_part_ptr, cellSize, nCellsPerSide, nParticles, h, restDensity, gasConstant, d_k);
         checkCuda(cudaGetLastError(), __LINE__ - 1);
 
-        // calculate forces
         updateAcceleration<<<blks, thPerBlk>>>(d_particles, d_cell_ptr, d_part_ptr, cellSize, nCellsPerSide, h, nParticles, viscosityCoefficient, gravity, d_k);
         checkCuda(cudaGetLastError(), __LINE__ - 1);
 
-        // symplectic Euler Integration
         updateState<<<blks, thPerBlk>>>(d_particles, dt, nParticles);
         checkCuda(cudaGetLastError(), __LINE__ - 1);
 
         // copy particles back to host for VTK output
         if (step % vtkOutputFrequency == 0)
         {
-            // checkCuda(cudaMemcpy(particles, d_particles, nParticles * sizeof(Particle), cudaMemcpyDeviceToHost), 12);
             particles = d_particles_vec;
-            // writeVTK(particles, step, nParticles);
             writeVTK(particles, step, nParticlesFluid);
             std::cout << "Step " << step << " done." << std::endl;
         }
